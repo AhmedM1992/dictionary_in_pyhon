7f368ad519b196a5fad9aa58bb6606c665f81c52
Ahmed Al-Kanan <32939040+AhmedM1992@users.noreply.github.com>
2018-01-07 20:34:34 +0200

    Update dict_and_keys.py

--- dict_and_keys.py
+++ dict_and_keys.py
@@ -1,4 +1,6 @@
-my_dict = {'name': 'ahmed', 'age':'25', 'country':'Ukraine', 'city':'Kharkiv', 'work':'QA'}
+my_dict = {'name': 'ahmed', 'age': '25', 'country': 'Ukraine', 'city': 'Kharkiv', 'work': 'QA'}
+
+
 def dict_1(slovar, keys):
     if keys in slovar:
         return slovar[keys]
